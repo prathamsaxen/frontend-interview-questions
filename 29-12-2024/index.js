e9cac0cd40363709a5d65c01141218505a53cf7c
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2024-12-29 20:33:56 +0530

    Added Question 2

--- 29-12-2024/index.js
+++ 29-12-2024/index.js
@@ -10,3 +10,15 @@ arr.forEach(num=>{
 })
 
 console.log(count);
+// Expected Output = 3
+// Reason = Conditional statement will assume zero as false and in that particular iteration code will not go inside that.
+
+
+
+// Question - 2
+const newArr = [1,2,4,5,6,7];
+console.log(newArr.slice(0,4));
+// Expected Output = [ 1, 2, 4, 5 ]
+// Reason = It never modifies the original array it returns the copy of array and first is the starting index and last parameter is the end index
+//          If you give the negative parameters then it will take the index from the end of array.
+
