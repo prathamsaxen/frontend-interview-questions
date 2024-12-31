6251b36691fd234f9a648d5a58b4147e73e26264
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2024-12-31 12:25:21 +0530

    Let Added

--- 31-12-2024/index.js
+++ 31-12-2024/index.js
@@ -8,6 +8,9 @@ if (a === "Hello World") {
 }
 console.log(a);
 console.log(b);
+// Global Scope
+// Reassign possible, Redeclaration possible
+
 
 // About let keyword
 let c = 10;
@@ -18,3 +21,14 @@ let c = 10;
         let d=20;
     }
 // console.log(d);
+// BlocK Scope
+// // Reassign possible but Redeclaration not possible
+
+
+// About const keyword
+const abc = 'Hello World';
+// abc = 5;
+// const abc = 10;
+console.log(abc);
+// BloCk Scope
+// Reassign not possible Redeclaration not possible
