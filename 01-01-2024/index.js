b9252ee300dcc65076afda9f3296074258adf948
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-01-01 12:49:30 +0530

    Added Template Literals

---
+++ 01-01-2024/index.js
@@ -0,0 +1,9 @@
+// Template Literals
+// Template Literals are the feature of ES6 which avoids the string concatenation
+
+const name = 'Pratham Saxena';
+const str = 'Hello ' + name;
+console.log(str);
+
+const str2 = `Hello ${name}`;
+console.log(str2);
