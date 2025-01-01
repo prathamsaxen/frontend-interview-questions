72ec45b9658ef9025db427cf9fc7d08ad319b8a1
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-01-01 13:02:25 +0530

    Functions Syntax Added

--- 01-01-2024/index.js
+++ 01-01-2024/index.js
@@ -7,3 +7,37 @@ console.log(str);
 
 const str2 = `Hello ${name}`;
 console.log(str2);
+
+
+// Functions 
+// 1. Traditional Functions
+// 2. Arrow Functions 
+// Difference Between Them -
+// a. Syntax
+
+
+
+
+
+// Syntax - 
+// Traditional Function
+function tempFunction(){
+    console.log("Traditional Function Callled");
+}
+
+
+// Arrow Function 
+const tempArrowFunction=()=>{
+    console.log("Arrow Function Callled");
+}
+
+// Call
+tempFunction();
+tempArrowFunction();
+
+
+
+
+
+console.log(`Type of Temp Function ----------> ${typeof tempFunction}`)
+console.log(`Type of Temp Arrow Function ----------> ${typeof tempArrowFunction}`)
