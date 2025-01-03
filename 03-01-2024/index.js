b602b2653039c408b4d987276d062af1e4528ca4
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-01-03 15:53:52 +0530

    Added Question 2

--- 03-01-2024/index.js
+++ 03-01-2024/index.js
@@ -1,2 +1,15 @@
+// Question - 1
 console.log("" > -1);
 console.log('' === true)
+//Expected Output = true
+                    // false
+                    // Reason = Empty strings considered as false.
+
+
+
+
+// Question - 2
+console.log('greater'>'elephant');
+// Expected Output = true
+//  Reason = It compares the first letter of string according to alphabeticall reason.
+
