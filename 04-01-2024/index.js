2792da6a8fb38227ecd39bdb8e884794aa848bdb
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-01-05 10:50:11 +0530

    Finite Currying Added

--- 04-01-2024/index.js
+++ 04-01-2024/index.js
@@ -1,11 +1,11 @@
-// Closures 
+// Closures
 
-function closuresInit(){
-    let data = 'Pratham Saxena';
-    function closureFunction(){
-        console.log(data);
-    }
-    return closureFunction;
+function closuresInit() {
+  let data = "Pratham Saxena";
+  function closureFunction() {
+    console.log(data);
+  }
+  return closureFunction;
 }
 
 let innerFunction = closuresInit();
@@ -15,3 +15,20 @@ innerFunction();
 // Reason: When we execute this code snippet, we might assume that the parent function has already completed and executed properly.
 // However, in this case, the inner function will use the elements from the parent function that are bound to it, rather than making copies of the variables.
 // This behavior binds the required variables by reference. This concept is known as a closure.
+
+// Currying Process
+
+function currying(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  };
+}
+
+const response = currying(5);
+const data = response(2);
+const finalOutput = data(19);
+console.log(finalOutput);
+console.log(currying(2)(5)(8));
+// Curryin works over closure if your function returns nested function which are taking arguments then instead of calling them again you can directly call them in same line with currying,
