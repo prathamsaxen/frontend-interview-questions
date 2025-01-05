d4174cd792f8193374635a6bf92c9eada2ffc47e
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-01-05 13:45:10 +0530

    Added Question 1

---
+++ 05-01-2024/index.js
@@ -0,0 +1,17 @@
+function sayHi() {
+    console.log(name);
+    // console.log(age);
+    var name = 'Lydia';
+    let age = 21;
+  }
+  
+  sayHi();
+
+
+//   Expected Output = Refference Error
+// Reason = var is declared with global scope and hoisting hoist it at the top so that is assigned with undefined keyword
+// Whereas in let it also hoist the variable but in this case the variable is in Temporal Dead Zone wher we can not access the variable
+// Temporal Dead Zone = The Time span variable hoist but not declared , in this time we can not access the variable.
+
+// console.log(data);
+// let data = 'Pratham Saxena';
